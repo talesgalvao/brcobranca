a38e15dcc5c0fe33f76051a91add79bf6e2ae0ab
Laerte Guimarães <31661772+laerte-guimaraes@users.noreply.github.com>
2019-10-21 19:21:31 -0300

    Update santander.rb

--- lib/brcobranca/boleto/santander.rb
+++ lib/brcobranca/boleto/santander.rb
@@ -1,23 +1,19 @@
 # -*- encoding: utf-8 -*-
+#
 # @author Kivanio Barbosa
 # @author Ronaldo Araujo
 module Brcobranca
   module Boleto
     class Santander < Base # Banco Santander
-      # Usado somente em carteiras especiais com registro para complementar o número do documento
-      attr_reader :seu_numero
-
+      validates_presence_of :convenio, message: 'não pode estar em branco.'
       validates_length_of :agencia, maximum: 4, message: 'deve ser menor ou igual a 4 dígitos.'
       validates_length_of :convenio, maximum: 7, message: 'deve ser menor ou igual a 7 dígitos.'
-      validates_length_of :numero_documento, maximum: 12, message: 'deve ser menor ou igual a 12 dígitos.'
-      validates_length_of :seu_numero, maximum: 7, message: 'deve ser menor ou igual a 7 dígitos.'
+      validates_length_of :nosso_numero, maximum: 7, message: 'deve ser menor ou igual a 7 dígitos.'
 
       # Nova instancia do Santander
       # @param (see Brcobranca::Boleto::Base#initialize)
       def initialize(campos = {})
-        campos = { carteira: '102',
-                   conta_corrente: '00000' # Obrigatória na classe base
-                  }.merge!(campos)
+        campos = { carteira: '102' }.merge!(campos)
         super(campos)
       end
 
@@ -28,6 +24,12 @@ module Brcobranca
         '033'
       end
 
+      # Número da conta corrente
+      # @return [String] 9 caracteres numéricos.
+      def conta_corrente=(valor)
+        @conta_corrente = valor.to_s.rjust(9, '0') if valor
+      end
+
       # Número do convênio/contrato do cliente junto ao banco. No Santander, é
       # chamado de Código do Cedente.
       # @return [String] 7 caracteres numéricos.
@@ -36,30 +38,25 @@ module Brcobranca
       end
 
       # Número sequencial utilizado para identificar o boleto.
-      # @return [String] 12 caracteres numéricos.
-      def numero_documento=(valor)
-        @numero_documento = valor.to_s.rjust(12, '0') if valor
-      end
-
-      # Número sequencial utilizado para identificar o boleto.
       # @return [String] 7 caracteres numéricos.
-      def seu_numero=(valor)
-        @seu_numero = valor.to_s.rjust(7, '0') if valor
+      def nosso_numero=(valor)
+        @nosso_numero = valor.to_s.rjust(7, '0') if valor
       end
 
       # Dígito verificador do nosso número.
       # @return [String] 1 caracteres numéricos.
       def nosso_numero_dv
-        nosso_numero = numero_documento.to_s.rjust(12, '0') unless numero_documento.nil?
-        nosso_numero.modulo11_2to9_santander
+        nosso_numero.modulo11(
+          multiplicador: (2..9).to_a,
+          mapeamento: { 10 => 0, 11 => 0 }
+        ) { |total| 11 - (total % 11) }
       end
 
       # Nosso número para exibir no boleto.
       # @return [String]
       # @example
-      #  boleto.nosso_numero_boleto #=> "000090002720-7"
+      #  boleto.nosso_numero_boleto #=> "9000272-7"
       def nosso_numero_boleto
-        nosso_numero = numero_documento.to_s.rjust(12, '0') unless numero_documento.nil?
         "#{nosso_numero}-#{nosso_numero_dv}"
       end
 
@@ -74,14 +71,13 @@ module Brcobranca
       # Segunda parte do código de barras.
       # 9(01) | Fixo 9 <br/>
       # 9(07) | Convenio <br/>
-      # 9(05) | Fixo 00000<br/>
-      # 9(08) | Nosso Numero<br/>
-      # 9(01) | IOF<br/>
+      # 9(13) | Nosso Numero Com DV<br/>
+      # 9(01) | IOF somente para seguradoras<br/> Fixo 9
       # 9(03) | Carteira de cobrança<br/>
       #
       # @return [String] 25 caracteres numéricos.
       def codigo_barras_segunda_parte
-        "9#{convenio}#{numero_documento}#{nosso_numero_dv}0#{carteira}"
+        "9#{convenio}00000#{nosso_numero}#{nosso_numero_dv}0#{carteira}"
       end
     end
   end
